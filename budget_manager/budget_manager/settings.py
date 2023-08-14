373f43818f212178338003e014a0aa67b53a955b
Mateusz Samek <samekmat@gmail.com>
2023-08-14 15:13:35 +0200

    Register app in settings

--- budget_manager/budget_manager/settings.py
+++ budget_manager/budget_manager/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'compressor',
+    "budget_manager_app",
+    "compressor",
 ]
 
 MIDDLEWARE = [

d06d9d0334a7d20620f90d4a25842bc54aaa0b45
Mateusz Samek <samekmat@gmail.com>
2023-08-17 17:19:05 +0200

    feat: Add models(Income, Expense, Categories, Currency and Tag) and configuration for them

--- budget_manager/budget_manager/urls.py
+++ budget_manager/budget_manager/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from budget_manager_app.views import index, RegisterView, LoginView
@@ -26,3 +28,6 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

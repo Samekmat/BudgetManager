723db34a27d6f25c46b979986fd39297fbc05078
Mateusz Samek <samekmat@gmail.com>
2023-08-17 13:58:44 +0200

    Add signal for creating profile into signals.py

--- budget_manager/budget_manager_app/apps.py
+++ budget_manager/budget_manager_app/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class BudgetManagerAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'budget_manager_app'
+
+    def ready(self):
+        import budget_manager_app.signals

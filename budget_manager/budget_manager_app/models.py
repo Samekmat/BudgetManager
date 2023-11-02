6a1350c3801ee46cd8fbc31bca0b691bb69bc1f5
Mateusz Samek <samekmat@gmail.com>
2023-11-02 22:54:55 +0100

    Add model Budget and set default amount of SavingGoal model as 0

--- budget_manager/budget_manager_app/models.py
+++ budget_manager/budget_manager_app/models.py
@@ -1,10 +1,38 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 from helper_models.models import Currency
 
+from expenses.models import Expense
+from incomes.models import Income
+
 
 class SavingGoal(models.Model):
     name = models.CharField(max_length=120)
-    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    amount = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     goal = models.DecimalField(max_digits=10, decimal_places=2)
     currency = models.ForeignKey(Currency, on_delete=models.CASCADE)
+
+
+class Budget(models.Model):
+    name = models.CharField(max_length=120)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='budgets',
+                             help_text="User who owns the budget")
+    shared_with = models.ManyToManyField(User, related_name='shared_budgets', blank=True,
+                                         help_text="Users with whom the budget is shared")
+    incomes = models.ManyToManyField(Income)
+    expenses = models.ManyToManyField(Expense)
+
+    def __str__(self):
+        return self.name
+
+    @property
+    def calculate_balance(self):
+        total_income = sum(income.amount for income in self.incomes.all())
+        total_expense = sum(expense.amount for expense in self.expenses.all())
+
+        total_result = total_income - total_expense
+        return total_result
+
+    class Meta:
+        ordering = ['-id']

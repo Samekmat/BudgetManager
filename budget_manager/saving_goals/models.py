8634b0a8b8702df2a9b0ea6cc257a9277c3e4fc5
Mateusz Samek <samekmat@gmail.com>
2023-12-07 19:27:01 +0100

    Add validators to specify the saving goal min value for amount and goal fields

--- budget_manager/saving_goals/models.py
+++ budget_manager/saving_goals/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator
 from django.db import models
 
 from helper_models.models import Currency
@@ -6,8 +7,8 @@ from helper_models.models import Currency
 
 class SavingGoal(models.Model):
     name = models.CharField(max_length=120)
-    amount = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-    goal = models.DecimalField(max_digits=10, decimal_places=2)
+    amount = models.DecimalField(max_digits=10, decimal_places=2, default=0, validators=[MinValueValidator(0)])
+    goal = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0)])
     currency = models.ForeignKey(Currency, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 

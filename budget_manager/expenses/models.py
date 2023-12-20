646595dd45eb92b811c0e253a56f73ef8bfc039d
samekmat <samekmat@gmail.com>
2023-12-20 12:00:49 +0100

    feat: update income and expense models by deleting null and blank for some fields

--- budget_manager/expenses/models.py
+++ budget_manager/expenses/models.py
@@ -6,17 +6,15 @@ from helper_models.models import Category, Currency, Tag
 
 class Expense(models.Model):
     amount = models.DecimalField(max_digits=10, decimal_places=2)
-    date = models.DateField(null=True, blank=True)
+    date = models.DateField()
     category = models.ForeignKey(
         Category,
         on_delete=models.CASCADE,
         limit_choices_to={"type": "expense"},
-        null=True,
-        blank=True,
     )
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     payment_method = models.CharField(max_length=30, choices=PAYMENT_METHOD_CHOICES, default="cash")
-    currency = models.ForeignKey(Currency, on_delete=models.CASCADE, null=True, blank=True)
+    currency = models.ForeignKey(Currency, on_delete=models.CASCADE)
     tags = models.ManyToManyField(Tag, blank=True)
     notes = models.TextField(null=True, blank=True)
     image = models.ImageField(upload_to="expense_images/", blank=True, null=True)

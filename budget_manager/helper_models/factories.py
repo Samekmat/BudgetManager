295625d3744982cd293cb982be258efe4bc872a0
samekmat <samekmat@gmail.com>
2023-12-21 01:03:34 +0100

    feat: add category and tag factories

--- budget_manager/helper_models/factories.py
+++ budget_manager/helper_models/factories.py
@@ -1,5 +1,5 @@
 import factory
-from helper_models.models import Currency
+from helper_models.models import Category, Currency, Tag
 
 
 class CurrencyFactory(factory.django.DjangoModelFactory):
@@ -9,3 +9,21 @@ class CurrencyFactory(factory.django.DjangoModelFactory):
     name = "US Dollar"
     code = "USD"
     symbol = "$"
+
+
+class CategoryFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Category
+
+    name = "Test Income Category"
+    type = "income"
+    builtin = False
+    user = factory.SubFactory("users.factories.UserFactory")
+
+
+class TagFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Tag
+
+    name = factory.Sequence(lambda n: f"test tag {n}")
+    user = factory.SubFactory("users.factories.UserFactory")

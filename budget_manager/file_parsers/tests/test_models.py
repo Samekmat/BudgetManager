83cea81903fadc216326c2d719838c0b9fa61592
samekmat <samekmat@gmail.com>
2024-02-02 19:59:23 +0100

    test: change date in csvfilemodetest

--- budget_manager/file_parsers/tests/test_models.py
+++ budget_manager/file_parsers/tests/test_models.py
@@ -1,4 +1,7 @@
+from datetime import datetime
+
 from django.test import TestCase
+from django.utils.timezone import make_aware
 from file_parsers.factories import CSVFileFactory
 from file_parsers.models import CSVFile
 from users.factories import UserFactory
@@ -7,7 +10,8 @@ from users.factories import UserFactory
 class CSVFileModelTest(TestCase):
     def setUp(self):
         self.user = UserFactory(username="testuser")
-        self.csv_file = CSVFileFactory(user=self.user, uploaded_at="2024-01-30")
+        self.upload_date = make_aware(datetime(year=2024, month=1, day=30))
+        self.csv_file = CSVFileFactory(user=self.user, uploaded_at=self.upload_date)
 
     def test_csv_file_str_method(self):
         self.assertEqual(str(self.csv_file), "testuser - 2024-01-30")
